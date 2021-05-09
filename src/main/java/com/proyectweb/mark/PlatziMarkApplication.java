df86e92001e1660a1cd1e8b71bc97893d2158b4a
JavierReivajG <jafg84alejandro@gmail.com>
2021-05-09 10:19:11 -0500

    Comenzando

---
+++ src/main/java/com/proyectweb/mark/PlatziMarkApplication.java
@@ -0,0 +1,13 @@
+package com.proyectweb.mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectweb/mark/PlatziMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectweb.mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
