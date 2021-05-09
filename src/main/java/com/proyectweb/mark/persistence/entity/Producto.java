95033ad115718da89d485abaa501c60ace527796
JavierReivajG <jafg84alejandro@gmail.com>
2021-05-09 16:20:18 -0500

    Agregando las variables de la base de datos

--- src/main/java/com/proyectweb/mark/persistence/entity/Producto.java
+++ src/main/java/com/proyectweb/mark/persistence/entity/Producto.java
@@ -1,7 +1,29 @@
 package com.proyectweb.mark.persistence.entity;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
-@Entity(name=)
+@Entity
+@Table(name="productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTIFY)
+    @Column (name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer IdCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Double cantidadStock;
 }
